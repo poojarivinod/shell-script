10cc3ac59f0bd6dac53dd5470c20514d1909c414
poojarivinod <vinod07081@gmail.com>
2025-07-13 13:40:28 +0530

    variables

--- 02-variable.sh
+++ 02-variable.sh
@@ -4,4 +4,3 @@ echo "rahim:: hi robert"
 echo "robert:: hello rahim"
 echo "rahim:: how are u"
 echo "robert:: i am good,how are U"
-
---
+++ 03-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ PERSON1=Ramesh #no space between before and after equal
+ PERSON2=Suresh
+
+echo "$PERSON1:: hi $PERSON2"
+echo "$PERSON2:: hello $PERSON1"
+echo "$PERSON1:: how are u"
+echo "$PERSON2:: i am good,how are U"
