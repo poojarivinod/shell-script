3d5e081b4c604d9d32e8f2ee2606bd353f54d2c2
poojarivinod <vinod07081@gmail.com>
2025-07-13 20:58:46 +0530

    array

---
+++ 07-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+MOVIES= ("pushpa" "rrr" "devara")
+#index start from 0, size is 3
+
+echo "First movie:: ${MOVIES[0]}"
+echo "second movie:: ${MOVIES[1]}"
+echo "third movie:: ${MOVIES[2]}"
+
+echo "All the movies are:: ${MOVIES[@]}"
