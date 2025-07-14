b53a26e7315b6b03d46448b1d6bb9280c5c6fca6
poojarivinod <vinod07081@gmail.com>
2025-07-14 09:58:40 +0530

    special variables

--- 08-special-var.sh
+++ 08-special-var.sh
@@ -3,7 +3,7 @@
 echo "All variables passed: $@"
 echo "Number of variables: $#"
 echo "script name: $0"
-echo "present working directory: $pwd"
+echo "present working directory: $PWD"
 echo "home directory of current user: $HOME"
 echo "which user is running this script: $USER"
 echo "process id of current script: $$"
