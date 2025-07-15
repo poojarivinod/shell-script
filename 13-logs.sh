af56fd191ed6fc0067728e72e74f2e7e630be9d3
poojarivinod <vinod07081@gmail.com>
2025-07-15 11:18:30 +0530

    log-file

--- 13-logs.sh
+++ 13-logs.sh
@@ -6,7 +6,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m" #it has to given, other wise previous mentioned color will contine 
 
-LOG_FOLDER="/var/log/shell-script" # /var/log/shell-script.log 
+LOG_FOLDER="/var/log/shell-script.log" # /var/log/shell-script.log 
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H:%M:%S)
 LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
