2abe9a4d873c8564f8ec3ef9d26af79254ba2114
poojarivinod <vinod07081@gmail.com>
2025-07-15 10:09:05 +0530

    log-file

--- 13-logs.sh
+++ 13-logs.sh
@@ -6,6 +6,11 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m" #it has to given, other wise previous mentioned color will contine 
 
+LOG_FOLDER="/var/log/shell-script.log"
+LOG_FILE=$(echo $0 | cut -d "-" -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H:%M:%S)
+LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
 VALIDATE(){
      if [ $1 -ne 0 ]
  then
@@ -16,25 +21,31 @@ VALIDATE(){
  fi
 }
 
+echo "script started executing at: $TIMESTAMP" &>>LOG_FILE_NAME
+
 if [ $USERID -ne 0 ]
 then
     echo "ERROR:: you must have sudo access to execute this script"
     exit 1 # other than 0
 fi
 
-dnf list installed mysql
+dnf list installed mysql &>>LOG_FILE_NAME
+
     if [ $? -ne 0 ]
 then
     dnf install mysql -y
-    VALIDATE $? "INSTALLING MYSQL"
+    VALIDATE $? "INSTALLING MYSQL" &>>LOG_FILE_NAME
+
 else
     echo -e " mysql is already .....$Y installed $N" 
 fi
 
-dnf list installed git
+dnf list installed git &>>LOG_FILE_NAME
+
     if [ $? -ne 0 ]
 then
-    dnf install git -y
+    dnf install git -y &>>LOG_FILE_NAME
+
     VALIDATE $? "INSTALLING Git"
 else 
     echo  -e " git is already .....$Y installed  $N"
