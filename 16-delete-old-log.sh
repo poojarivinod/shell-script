961b6fb26b4ecdb2b4b946f12f8c2d96649a04f4
poojarivinod <vinod07081@gmail.com>
2025-07-16 19:08:48 +0530

    delete old logs

--- 16-delete-old-log.sh
+++ 16-delete-old-log.sh
@@ -4,3 +4,8 @@ SOURCE_DIR="/home/ec2-user/app-log" #/home/ec2-user/app-log
 
 FILES_TO_DELETE=$(find $SOURCE_DIR -name "*log" -mtime +14)
 echo "files to be deleted: $FILES_TO_DELETE"
+
+while read -r file
+do 
+   echo "deleting file $file"
+done < FILES_TO_DELETE
