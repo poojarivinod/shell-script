18b74905ad54dbeef89f1be5fe7c985b03be1cb2
poojarivinod <vinod07081@gmail.com>
2025-07-17 12:34:30 +0530

    backup

--- 18-backup-script.sh
+++ 18-backup-script.sh
@@ -9,7 +9,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m" #it has to give, other wise previous mentioned color will contine 
 
-LOG_FOLDER="/home/ec2-user/shell-scrip-log" # /var/log/shell-script.log 
+LOG_FOLDER="/home/ec2-user/shell-scrip-log"  
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H:%M:%S)
 LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
@@ -24,5 +24,16 @@ if [ $# -lt 2 ]
          USAGE
 fi         
 
+if [ ! -d $SOURCE_DIR ]
+then
+     echo -e " $SOURCE_DIR Does not exist ......please check"
+     exit 1
+fi
+
+if [ ! -d $DEST_DIR ]
+then
+     echo -e " $DEST_DIR Does not exist ......please check"
+     exit 1
+fi
 mkdir -p /home/ec2-user/shell-scrip-log
 echo "script started executing at: $TIMESTAMP" &>> $LOG_FILE_NAME
