10d0f60bac502611e08a93715c5ba893aff757a7
poojarivinod <vinod07081@gmail.com>
2025-07-18 09:14:49 +0530

    backend

--- 18-backup-script.sh
+++ 18-backup-script.sh
@@ -9,7 +9,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m" #it has to give, other wise previous mentioned color will contine 
 
-LOG_FOLDER="/home/ec2-user/shell-scrip-log"  
+LOG_FOLDER="/home/ec2-user/shell-script-log"  
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H:%M:%S)
 LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
