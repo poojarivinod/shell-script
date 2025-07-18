a41c263f6522ed58259d392a34d55062bea54c01
poojarivinod <vinod07081@gmail.com>
2025-07-18 19:38:47 +0530

    disk usage

--- 20-disk-usage.sh
+++ 20-disk-usage.sh
@@ -6,6 +6,6 @@ DISK_THRESHOLD=5     #in real project it is 80%
 while read -r line
 do
     USAGE=$(echo $line | awk -F " " '{print $6F}')
-    PARTITION=$(echo $line | awk -F " " '{PRINT $NF}')
+    PARTITION=$(echo $line | awk -F " " '{PRINT $7F}')
     echo "Partition: $PARTITION , Usage: $USAGE"
 done <<< $DISK_USAGE  
