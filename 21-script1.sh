448c6fa7901551941ab04e602ec267ac10fb350f
poojarivinod <vinod07081@gmail.com>
2025-07-19 11:43:40 +0530

    disk usage

--- 21-script1.sh
+++ 21-script1.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 MSG="SCRIPT1"
-GREET="Hello from SCRIPT1"
+GREET="HI from SCRIPT1"
+source ./22-script2.sh
+
 echo "Hello world: $MSG"
 
-sh 22-script2.sh
+#sh 22-script2.sh
--- 22-script2.sh
+++ 22-script2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 MSG="SCRIPT2"
+GREET="HI from SCRIPT1"
 
 echo "Hello world: $MSG"
 echo "Greeting: $GREET"
