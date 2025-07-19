4fa53999539046b2aa508e8cc1998729f25a9361
poojarivinod <vinod07081@gmail.com>
2025-07-19 11:57:56 +0530

    disk usage

--- 21-script1.sh
+++ 21-script1.sh
@@ -5,5 +5,6 @@ GREET="HI from SCRIPT1"
 source ./22-script2.sh
 
 echo "Hello world: $MSG"
+echo "A value= $A"
 
 #sh 22-script2.sh
--- 22-script2.sh
+++ 22-script2.sh
@@ -2,6 +2,7 @@
 
 MSG="SCRIPT2"
 GREET="HI from SCRIPT1"
-
+A:"10"
+ 
 echo "Hello world: $MSG"
 echo "Greeting: $GREET"
