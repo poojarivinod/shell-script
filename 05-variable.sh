7630c1a0c1571d09c093c81a895ce9f0d8301f02
poojarivinod <vinod07081@gmail.com>
2026-01-07 12:27:37 +0530

    shell-script

--- 05-variable.sh
+++ 05-variable.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 echo "please enter your username::"
-read -s USERNAME
+read -s USERNAME # if we remove (-s), then we can see the username while typing, otherwise can't 
 echo "username entered: $USERNAME"
 echo "please enter your password::"
 read -s PASSWORD
